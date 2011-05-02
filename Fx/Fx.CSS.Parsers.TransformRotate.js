661f71ec86209536c7cd461385db405b7e8e16f0
Antoine Goutenoir <antoine.goutenoir@gmail.com>
2011-05-02 16:12:15 +0200

    Support for negative degrees... So. Damn. Obvious.

--- Fx/Fx.CSS.Parsers.TransformRotate.js
+++ Fx/Fx.CSS.Parsers.TransformRotate.js
@@ -1,14 +1,18 @@
 /**
  * Allows for smooth Mootools Fx with rotate CSS3 property
  *
- * @found b22222.com/test/testcss3.html
+ * @origin http://b22222.com/test/testcss3.html
+ * @source https://github.com/Nutellove/mootools-cloud
+ * @require MooTools 1.3+
+ * @licence GNU/GPL
+ * @version 0.1
  */
 
 Fx.CSS.Parsers.extend({
  
 	TransformRotate: {
 		parse: function(value){
-			return ((value = value.match(/^rotate\(([0-9]{0,3})deg\)$/i))) ? value[1] : false;
+			return ((value = value.match(/^rotate\((-?[0-9]{0,3})deg\)$/i))) ? value[1] : false;
 		},
 		compute: function(from, to, delta){
 			return Math.round(Fx.compute(from, to, delta));
