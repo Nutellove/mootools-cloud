21e87695b6cdf1f064656533b31b80ae2dcb99c5
Antoine Goutenoir <antoine.goutenoir@gmail.com>
2011-08-03 18:20:26 +0200

    Added maxUsages

--- Function/Function.maxUsages.js
+++ Function/Function.maxUsages.js
@@ -1,6 +1,7 @@
 /*
 ---
-description: Makes sure this function will only be used at most naxUsages times. A disposable function, like a bic!
+description: Makes sure this function will only be used at most maxUsages times.
+             A disposable function, like a bic!
 
 authors:
   - Antoine Goutenoir <antoine@goutenoir.com>
@@ -18,6 +19,7 @@ provides:
 */
 Function.implement({
   maxUsages: function(maxUsages) {
+    if (!maxUsages) maxUsages = 1;
     var nbUsages = 1;
     return function(){
       if (nbUsages++ <= maxUsages) return this.apply(this, arguments);
