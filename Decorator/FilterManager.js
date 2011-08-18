2dfa762831b7dc5a00fbe1c391fe614420bbc12a
Antoine Goutenoir <antoine.goutenoir@gmail.com>
2011-08-18 14:26:27 +0200

    FilterManager v1.2 - Moved to Decorator - Added empty handle management

--- Decorator/FilterManager.js
+++ Decorator/FilterManager.js
@@ -1,6 +1,6 @@
 /*
 ---
-description: FilterManager v1.1
+description: FilterManager v1.2
              Manages the handles that will filter the container's elements
              The rel attribute of the handle must match the class of an element for this element to be shown
              If the rel is empty, all elements are shown
@@ -26,7 +26,9 @@ FilterManager = new Class({
   options: {
     handleActivationEvent: 'click',
     handleActivatedClass:  'active',
-    handleTaggingProperty: 'rel'
+    handleEmptyClass:      'empty',
+    handleTaggingProperty: 'rel',
+    stopEventPropagation:   true // Stops propagation on the handle activation event
     //onBeforeHide: Function.from
     //onAfterHide:  Function.from
     //onBeforeShow: Function.from
@@ -38,31 +40,42 @@ FilterManager = new Class({
     this.handles   = $$(handles);
     this.setOptions(options);
 
+    this.allElements = this.container.getChildren();
+
     this.addHandlesBehavior();
   },
 
   addHandlesBehavior: function () {
     var that = this;
     this.handles.each(function(handle){
+      // Attach the activation event
       handle.addEvent(this.options.handleActivationEvent, function(event){
+        if (that.options.stopEventPropagation) event.stop();
         if (!this.hasClass(that.options.handleActivatedClass)) {
           var rel = this.getProperty(that.options.handleTaggingProperty);
           that.filterContent (rel);
-          that.handles.removeClass(that.options.handleActivatedClass);
-          this.addClass(that.options.handleActivatedClass);
+          if (that.options.handleActivatedClass) {
+            that.handles.removeClass(that.options.handleActivatedClass);
+            this.addClass(that.options.handleActivatedClass);
+          }
         }
       });
+      // Add the handleEmptyClass if necessary
+      if (this.options.handleEmptyClass &&
+          !this.hasContentForTagExpression(handle.getProperty(this.options.handleTaggingProperty)))
+      {
+        handle.addClass(this.options.handleEmptyClass);
+      }
     }.bind(this));
 
     return this;
   },
 
   filterContent: function (tagExpression) {
-    // Get elements that pass the filtering
-    var elementsToShow = this.getContentForTag(tagExpression);
+    // Get elements that pass the tagExpression filtering
+    var elementsToShow = this.getContentForTagExpression(tagExpression);
     // Hide all elements except those that pass
-    var allElements = this.container.getChildren();
-    var elementsToHide = allElements.filter(function(item){return !elementsToShow.contains(item)});
+    var elementsToHide = this.allElements.filter(function(item){return !elementsToShow.contains(item)});
     // Fire onBeforeHide Event
     this.fireEvent('beforeHide');
     // Hide elements that don't pass the filtering
@@ -77,16 +90,33 @@ FilterManager = new Class({
     this.fireEvent('afterShow');
   },
 
+  getContentForTagExpression: function (tagExpression) {
+    var elements;
+    if (tagExpression == null) {
+      // If empty, get all
+      elements = this.allElements;
+    } else {
+      // Else, get elements that pass the filtering
+      elements = this.getContentForTag(tagExpression);
+    }
+
+    return elements;
+  },
+
   getContentForTag: function (tag) {
     return this.container.getChildren('.'+tag);
   },
 
+  hasContentForTagExpression: function (tagExpression) {
+    return (this.getContentForTagExpression(tagExpression).length > 0);
+  },
+
   hideElements: function (elements) {
-    elements.addClass('hidden');
+    elements.setStyle('display', 'none');
   },
 
   showElements: function (elements) {
-    elements.removeClass('hidden');
+    elements.setStyle('display', 'block');
   }
 
 });
